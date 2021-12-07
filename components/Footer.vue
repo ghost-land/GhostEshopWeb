3626470169d7c696d3cfa02146bbbe00e6ef6642
Ghost0159 <contact.ghost01@gmail.com>
2021-12-07 22:36:50 +0100

    Adding Twitter, JSON, RSS and Wiki icon
    Adding Twitter, JSON, RSS and Wiki icon on footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -9,13 +9,25 @@
       <v-card-text>
         <v-btn
           class="mx-4 white--text"
-          href="https://github.com/ghost-land"
+          href="https://discord.gg/a7vdfT6YPq"
           icon
         >
+          <v-icon size="24px"> mdi-discord </v-icon>
+        </v-btn>
+        <v-btn class="mx-4 white--text" href="https://twitter.com/GhosteShop" icon>
+          <v-icon size="24px"> mdi-twitter </v-icon>
+        </v-btn>
+        <v-btn class="mx-4 white--text" href="https://github.com/ghost-land" icon>
           <v-icon size="24px"> mdi-github </v-icon>
         </v-btn>
-        <v-btn class="mx-4 white--text" href="https://discord.gg/a7vdfT6YPq" icon>
-          <v-icon size="24px"> mdi-discord </v-icon>
+        <v-btn class="mx-4 white--text" href="https://raw.githubusercontent.com/ghost-land/ghost-land.github.io/main/ghosteshop.json" icon>
+          <v-icon size="24px"> mdi-code-json </v-icon>
+        </v-btn>
+        <v-btn class="mx-4 white--text" href="https://ghosteshop.com/feed/news.xml" icon>
+          <v-icon size="24px"> mdi-rss </v-icon>
+        </v-btn>
+        <v-btn class="mx-4 white--text" href="https://wiki.ghosteshop.com/" icon>
+          <v-icon size="24px"> mdi-book-open-page-variant </v-icon>
         </v-btn>
       </v-card-text>
 
