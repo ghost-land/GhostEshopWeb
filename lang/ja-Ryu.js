266ff7741b7ba4aba0dd2d0a69b5581c5408d913
kuragehime <kuragehime641@gmail.com>
2022-07-28 16:32:30 +0900

    Added Ryukyuan languages
    Ryukyuan languages have been added

--- lang/ja-Ryu.js
+++ lang/ja-Ryu.js
@@ -1,1 +1,56 @@
-
+export default {
+    home: {
+      title_1: 'Ghost eShopとー？',
+        paragraphe_1:
+          'Ghost eShopー、Nintendo 3DSぬゲームあちみたるデータベースやいびーん。GhosteShopがあれー、うんじゅが3DSー時代遅りんかえーなやびらん。くぬサイトてぃがろー3DS用Ghost eShopアプリから、ゲームダウンロードするくとぅがなやびーん。',
+      title_2: 'Ghost eShop3DS(New3DS)んかいインストールするたみぬ条件ーぬーやいびーが？',
+        paragraphe_2:
+          'Ghost eShopーみーなとーいびーしが、インストールすんでーいくちがぬ条件ぬあいびーん。',
+          paragraphe_2_1:
+            '- カスタムファームウェア（Luma3DSんでー...）インストールさる3DSてぃがろーNew3DS',
+          paragraphe_2_2:
+            '- 10MB以上ぬ空き容量ぬあるSDカードてぃがろーMicroSDカード',
+          paragraphe_2_3:
+            '- 以下っしダウンロード可能なGhost eShopぬ最新バージョン',
+      title_3:
+        'Ghost eShopんかいねーんゲーモーちゃーしぇーゆたさいびーが？',
+        paragraphe_3:
+          'Ghost eShopんかいねーんゲーモー、以下ぬフォームからリクエストなやびーん。',
+      title_4:
+        '3DSぬGhost eShopアプリちからじんかいゲームダウンロードすしぇー可能やいびーが？',
+        paragraphe_4:
+          '3DSぬGhost eShopアプリちからじんかいゲームダウンロードすんがー、ゲームぬQRコード読み取いん（FBI経由っしインストールするばー）くとぅがなやびーしが、くぬ公式ウェブサイトからダウンロードすしんなやびーん。',
+      faq:'ゆーある質問:',
+    },
+    games: {
+      search: '検索',
+      inputhere: 'くまんかい挿入',
+      title: 'タイトル',
+      icon: 'アイコン',
+      author: 'ちゅくやー',
+      description: '説明',
+      version: 'バージョン',
+      console: 'コンソール',
+      category: 'カテゴリー',
+      last_updated: '最終更新日',
+      rowsperpage: '1ページあたりぬ行数',
+      size: '容量',
+      list: 'ゲームリスト',
+      subtitle: '現在Ghost eShopんかいあるまじりぬゲームぐ紹介さびーん。',
+      notspecified: '見ちかやびらんたん',
+    },
+    header: {
+      home: 'ホーム',
+      games: 'ゲーム',
+      request: 'ゲームリクエスト',
+      french: 'フランス語',
+      english: '英語',
+      spanish: 'スペイン語',
+      japanese: 'やまとぅぐち',
+    },
+    footer: {
+      copyright: '© Ghost eShop - 全著作権所有',
+    },
+    download: 'ダウンロード',
+    close: 'くーいん',
+  }
